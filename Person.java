1bd4237c7e0e699e8095cc53b0cb05cec672196a
Abderrahmen Belalem <55656596+abderrahmen-belalem@users.noreply.github.com>
2019-12-27 19:37:55 +0100

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,55 @@
+public class Person {
+    int age;
+    String firstName;
+    String lastName;
+    String birthdayDate;
+    String birthdayPlace;
+
+    Person(int age, String firstName, String lastName, String birthdayDate, String birthdayPlace) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayDate = birthdayDate;
+        this.birthdayPlace = birthdayPlace;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(String birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+
+    public String getBirthdayPlace() {
+        return birthdayPlace;
+    }
+
+    public void setBirthdayPlace(String birthdayPlace) {
+        this.birthdayPlace = birthdayPlace;
+    }
+}
